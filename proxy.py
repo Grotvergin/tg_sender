9d01c0d4a012ef5674bea54a558e21f7c3c7868d
Roma <mishenin_r@mail.ry>
2024-12-08 00:16:07 +0300

    finish?

--- proxy.py
+++ proxy.py
@@ -15,18 +15,9 @@ def getProxyByComment(user_id, comment):
                 if proxy_data.get('descr') == comment:
                     Stamp('Found a free proxy', 's')
                     BOT.send_message(user_id, f"🟩 Нашёл свободный прокси {proxy_data['host']}:{proxy_data['port']}")
-                    http_proxy = {
-                        'http': f"http://{proxy_data['user']}:{proxy_data['pass']}@{proxy_data['host']}:{proxy_data['port']}",
-                        'https': f"http://{proxy_data['user']}:{proxy_data['pass']}@{proxy_data['host']}:{proxy_data['port']}",
-                    }
-                    socks_proxy = (
-                        2,
-                        proxy_data['host'],
-                        int(proxy_data['port']),
-                        True,
-                        proxy_data['user'],
-                        proxy_data['pass'],
-                    )
+                    address = f"http://{proxy_data['user']}:{proxy_data['pass']}@{proxy_data['host']}:{proxy_data['port']}"
+                    http_proxy = {'http': address, 'https': address}
+                    socks_proxy = (2, proxy_data['host'], int(proxy_data['port']), True, proxy_data['user'], proxy_data['pass'])
                     return http_proxy, socks_proxy, proxy_id
             Stamp('No free proxies', 'e')
             BOT.send_message(user_id, '🟥 Нет свободных прокси')
